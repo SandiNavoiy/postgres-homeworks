fcae44d382883c3f6b68df7be7337c206f9e7008
Sandi_Navoiy <santes.au@gmail.com>
2023-05-08 20:37:10 +0300

    3

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,25 +1,19 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import csv
 import os
-
-
-class InstantiateCSVError:
-    pass
-
+import psycopg2
 
 def instantiate_from_csv(file_mame):
-    """Открытие файла csv"""
-    all = []
-    if not os.path.exists(file_mame):
-        raise FileNotFoundError('Отсутствует файл item.csv')
-
+    """"""
     with open(os.path.join(file_mame), 'r') as f:
         reader = csv.reader(f)
         next(reader)
+        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        cur = conn.cursor()
         for row in reader:
-            if len(row) != 3:
-                raise InstantiateCSVError("Файл item.csv поврежден")
-            name = row[0]
-            price = int(row[1])
-            quantity = int(row[2])
-            cls(name, price, quantity)
+            cur.execute("INSERT INTO my_table (column1, column2, column3) VALUES (%s, %s, %s)",
+                        (row[0], row[1], row[2]))
+
+    conn.commit()
+    cur.close()
+    conn.close()
