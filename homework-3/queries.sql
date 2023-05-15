4d87b235bea2f80e62af917553808bd4db9584f7
Sandi_Navoiy <santes.au@gmail.com>
2023-05-15 19:04:53 +0300

    1+

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,10 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT customers.company_name, employees.last_name
+FROM customers
+JOIN orders ON customers.customer_id = orders.customer_id
+JOIN employees ON orders.employee_id = employees.employee_id
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
