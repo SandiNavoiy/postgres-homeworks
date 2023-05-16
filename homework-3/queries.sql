d331a083c31c3352c9f276e23a2b92e44b292b3a
Sandi_Navoiy <santes.au@gmail.com>
2023-05-16 22:37:03 +0300

    3 ДЗ готова

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,10 +1,10 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-Select customers.company_name as customer, CONCAT(employees.first_name, ' ', employees.last_name) as employee
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) as employee
 From customers
-JOIN orders ON orders.customer_id = customers.customer_id
-JOIN employees ON orders.employee_id = employees.employee_id
+JOIN orders USING (customer_id)
+JOIN employees USING (employee_id)
 JOIN shippers ON orders.ship_via = shippers.shipper_id
 WHERE customers.city = 'London' AND  employees.city = 'London' AND shippers.company_name = 'United Package'
 
@@ -14,7 +14,14 @@ WHERE customers.city = 'London' AND  employees.city = 'London' AND shippers.comp
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 SELECT product_name, units_in_stock, contact_name, phone FROM products
 INNER JOIN suppliers USING(supplier_id)
-WHERE products.discontinued < 1 AND products.units_in_stock < 25
+WHERE
+	products.discontinued = 0
+	AND products.units_in_stock < 25
+	AND products.category_id IN
+		(SELECT category_id
+		 FROM categories
+		 WHERE category_name IN ('Dairy Products', 'Condiments')
+		 )
 ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
@@ -25,8 +32,10 @@ WHERE customer_id NOT IN (SELECT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
-Select product_name FROM products
+SELECT product_name
+FROM products
 WHERE product_id IN (
-	SELECT product_id FROM order_details
-	GROUP BY product_id
-	HAVING SUM(quantity) = 10
+	SELECT product_id
+	FROM order_details
+	WHERE quantity = 10
+)
